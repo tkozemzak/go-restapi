82e94cb9e5d89b515a387e9766a4595d84938d90
Tim Kozemzak <timkozemzak@gmail.com>
2022-02-22 18:43:35 -0700

    adds create and delete functions

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
-
+	"math/rand"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -22,19 +24,40 @@ type Author struct {
 	Lastname	string `json:"lastname"`
 }
 
+//init books var as a slice Book struct
+var books []Book
+
 //get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("/api/books")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 //get single book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //get params
+
+	//loop through books and find correct id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 
 }
 
 //create new book
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
 
+	book.ID = strconv.Itoa(rand.Intn(10000000)) //mock id
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 //update book
@@ -44,7 +67,15 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 
 //delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+		books = append(books[:index], books[index+1:]...)
+		break	
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 
@@ -52,7 +83,11 @@ func main() {
 	//initialize router
 	router := mux.NewRouter()
 
-	//mock data
+	//mock data - todo: implement DB
+	books = append(books, Book{ID: "1", Isbn: "243235", Title: "Book 1", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "343235", Title: "Book 2", Author: &Author{Firstname: "Bill", Lastname: "Murray"}})
+	books = append(books, Book{ID: "3", Isbn: "543235", Title: "Book 3", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
+
 
 	//create route handlers/endpoints
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
@@ -62,5 +97,8 @@ func main() {
 	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 	fmt.Println("Routes created")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	port := ":8000"
+
+	fmt.Println("Server starting on port", port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
